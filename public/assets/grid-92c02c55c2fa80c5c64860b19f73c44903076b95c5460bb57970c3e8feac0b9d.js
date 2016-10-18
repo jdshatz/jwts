ac33ae149faa3666e851dbac64753c2423155c5c
J.D <jshatz@optonline.net>
2016-10-18 15:57:55 -0400

    public file fix

---
+++ public/assets/grid-92c02c55c2fa80c5c64860b19f73c44903076b95c5460bb57970c3e8feac0b9d.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+   $("td").on(
+        "mouseenter", function(){
+         $(this).fadeTo(500, 0.5);
+         $(this).children("h3").fadeIn(500);
+      }).on("mouseleave", function(){
+      $(this).fadeTo(500, 1); //How to remove text?
+      $("h3").fadeOut(500);
+  });
+ })
+
+;
---
+++ public/assets/jwts-aabbe893d5a2ff5f266afb42bf085c45c8ad14d33abaf7daf65a0e42114f152d.js
@@ -0,0 +1,31 @@
+$(document).on('turbolinks:load',function(){
+  event.preventDefault();
+  $('.menutab').click(function(){
+    event.preventDefault();
+    var toggling = $('.mainmenu').css('display');
+      if (toggling = 'none'){
+        $('.mainmenu').slideToggle(1000);
+        $('.navigation').animate({bottom: "-=150px"});
+        toggling = 'block';
+      }
+      else{
+       $('.mainmenu').slideToggle(1000);
+        $(this).css({'z-index':'0', 'opacity':'1'}, 4)
+        $('.navigation').animate({bottom: "+=150px"});
+        toggling = 'none';
+        $("body").css({'z-index':'1', 'opacity':'1'})
+      }
+  })
+})
+
+
+   //$("a").on('click', function(){
+   // console.log('click'); //The rest of the commands, at present, are not working.  If they are, they are being reverted to the show# view.
+    //$(".wrapper").fadeTo(200,0.2); //opacity
+    //$(".hidden").css("display","inline").fadeIn(5000,0.8);
+    //$(".hidden").on('click', function(event) {
+      //$(".hidden").fadeOut(200).remove(); //remove put in there to confirm that it will disappear from the page - opposite of 'append'
+      //$(".wrapper").fadeTo(200,1);
+    //});
+ // });
+
